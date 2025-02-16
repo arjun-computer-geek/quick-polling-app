157c3cfe15b93702f1a3293e3c29f9fa8f4174ef
Arjun <arjun2000raj@gmail.com>
2025-02-16 13:26:41 +0530

    bckedn cors issue

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
     cors: {
-        origin: "*",
+        origin: "https://quick-polling-app.vercel.app/",
         methods: ["GET", "POST"]
     }
 });
