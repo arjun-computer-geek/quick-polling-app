39dbe64d8ea76966eb98d24836f8ee896e40b42e
Arjun <arjun2000raj@gmail.com>
2025-02-16 12:05:25 +0530

    Poling feate created

---
+++ backend/models/poll.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const pollSchema = new mongoose.Schema({
+    question: String,
+    options: [{
+        text: String,
+        votes: { type: Number, default: 0 }
+    }],
+    created_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Poll', pollSchema);
